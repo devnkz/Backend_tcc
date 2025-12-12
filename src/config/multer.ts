1377ca0b825945be66e444bd6ac96b213f05e23c
devnkz <vieiranyckolas1@gmail.com>
2025-12-12 00:54:49 -0300

    tmp/uploads

--- src/config/multer.ts
+++ src/config/multer.ts
@@ -1,9 +1,17 @@
 import multer from "fastify-multer";
-import path from "path";
 import crypto from "crypto";
+import fs from "fs";
+
+const uploadPath = "/tmp/uploads";
+
+if (!fs.existsSync(uploadPath)) {
+  fs.mkdirSync(uploadPath, { recursive: true });
+}
 
 const storage = multer.diskStorage({
-  destination: path.resolve(__dirname, "..", "..", "uploads"),
+  destination: (req, file, callback) => {
+    callback(null, uploadPath);
+  },
   filename: (req, file, callback) => {
     const fileHash = crypto.randomBytes(8).toString("hex");
     const fileName = `${fileHash}-${file.originalname}`;
